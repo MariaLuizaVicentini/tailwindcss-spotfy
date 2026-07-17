355e6a8a4519618b50273ea9e32146c2cbbfe50b
maluvicentini <vicentinimalu1@gmail.com>
2026-07-17 18:57:33 -0300

    feat: botoes do footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from "lucide-react";
+import { Home as HomeIcon, Repeat, SkipForward, Search, SkipBack, Shuffle, Library, ChevronLeft, ChevronRight, Play } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -127,8 +127,38 @@ export default function Home() {
         </main>
       </div>
 
-      <footer className="bg-zinc-0 border-zinc-700 p-6">
-        rodape
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <Image src="/album-hari-1.jpg" width={56} height={56} alt="Capa do album McHariel" />
+          <div className="flex flex-col">
+            <strong className="font-normal">Temporada 2026</strong>
+            <span className="text-xs text-zinc-400">Mc Hariel Artista</span>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center gap-6">
+            <Shuffle size={20} className="text-zinc-200 "/>
+            <SkipBack size={20} className="text-zinc-200 "/>
+
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
+              <Play />
+            </button>
+
+            <SkipForward size={20} className="text-zinc-200 "/>
+            <Repeat size={20} className="text-zinc-200 "/>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">0:31</span>
+            <span className="text-xs text-zinc-400">2:14</span>
+          </div>
+        </div>
+
+        <div>
+          
+        </div>
+
       </footer>
     </div>
   );
