7f1ee2f4e1636b61d6009badd3efe4617b105720
maluvicentini <vicentinimalu1@gmail.com>
2026-07-17 15:16:57 -0300

    organizando estrutura

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 
 export default function Home() {
   return (
-      <main>
-        <h1> Hello word</h1>
-      </main>
+    <div className=" h-screen flex flex-col">
+      <div className=" flex flex-1">
+        <aside className="bg-zinc-950 w-64 p-6">
+          sidebar
+        </aside>
+        <main className="flex-1 p-6">
+          Conteudo - main
+        </main>
+      </div>
+
+      <footer className="bg-zinc-800 border-zinc-700 p-6">rodape</footer>
+    </div>
   );
 }
